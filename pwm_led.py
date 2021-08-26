dfa84662002e9d08f3860eb765b20acb169d7495
LinZhang@UCA <88195432+linzhangUCA@users.noreply.github.com>
2021-08-25 21:49:13 -0500

    Update pwm_led.py

--- pwm_led.py
+++ pwm_led.py
@@ -1,8 +1,10 @@
 """
 Use RPi.GPIO library to realize a slowly blinking LED.
-Light up the LED gradually in 2 seconds, then turn it off gradually in 4 seconds.
+Light up the LED gradually in 2 seconds, then turn it off gradually in 1 seconds.
 Repeat this procedure until the program is stopped.
 Use the RPi.GPIO library ONLY.
+
+Hint: PWM
 
 Have fun!
 """
