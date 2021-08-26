d18137d49a6389bdf28ee53b968c13b2284f58b7
LinZhang@UCA <88195432+linzhangUCA@users.noreply.github.com>
2021-08-25 21:43:46 -0500

    Create pwm_led.py

---
+++ pwm_led.py
@@ -0,0 +1,18 @@
+"""
+Use RPi.GPIO library to realize a slowly blinking LED.
+Light up the LED gradually in 2 seconds, then turn it off gradually in 4 seconds.
+Repeat this procedure until the program is stopped.
+Use the RPi.GPIO library ONLY.
+
+Have fun!
+"""
+
+#### Do not change following 2 lines ####
+import RPi.GPIO
+import time
+#########################################
+
+
+#### Write your code below ####
+
+
